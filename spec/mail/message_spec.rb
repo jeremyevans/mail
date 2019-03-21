jeremyevans/mail

commit 404ecdd290f89658fb05da02ea5b6139fd82ebdb

Improve multipart charset handling and Content-* conversion
* Converting to multipart moves Content-* headers to the new part.
* Multipart Content-Type no longer includes a needless charset param.

Closes #1145

lib/mail/message.rb
@@ -1765,12 +1765,22 @@ module Mail
       self.attachments[basename] = filedata
     end
 
+    MULTIPART_CONVERSION_CONTENT_FIELDS = [ :content_description, :content_disposition, :content_transfer_encoding, :content_type ]
+    private_constant :MULTIPART_CONVERSION_CONTENT_FIELDS if respond_to?(:private_constant)
+
     def convert_to_multipart
-      text = body.decoded
-      self.body = ''
-      text_part = Mail::Part.new({:content_type => 'text/plain;',
-                                  :body => text})
+      text_part = Mail::Part.new(:body => body.decoded)
+
+      MULTIPART_CONVERSION_CONTENT_FIELDS.each do |field_name|
+        if value = send(field_name)
+          writer = :"#{field_name}="
+          text_part.send writer, value
+          send writer, nil
+        end
+      end
       text_part.charset = charset unless @defaulted_charset
+
+      self.body = ''
       self.body << text_part
     end
 
@@ -2038,7 +2048,6 @@ module Mail
 
     def add_multipart_alternate_header
       header['content-type'] = ContentTypeField.with_boundary('multipart/alternative').value
-      header['content_type'].parameters[:charset] = @charset
       body.boundary = boundary
     end
 
@@ -2046,7 +2055,6 @@ module Mail
       unless body.boundary && boundary
         header['content-type'] = 'multipart/mixed' unless header['content-type']
         header['content-type'].parameters[:boundary] = ContentTypeField.generate_boundary
-        header['content_type'].parameters[:charset] = @charset
         body.boundary = boundary
       end
     end
@@ -2054,7 +2062,6 @@ module Mail
     def add_multipart_mixed_header
       unless header['content-type']
         header['content-type'] = ContentTypeField.with_boundary('multipart/mixed').value
-        header['content_type'].parameters[:charset] = @charset
         body.boundary = boundary
       end
     end

spec/mail/message_spec.rb
@@ -1408,6 +1408,24 @@ describe Mail::Message do
         expect(mail.parts.last.content_transfer_encoding).to match(/7bit|8bit|binary/)
       end
 
+      describe "convert_to_multipart" do
+        subject do
+          read_fixture('emails', 'attachment_emails', 'attachment_only_email.eml').tap(&:convert_to_multipart)
+        end
+
+        it "original content headers move to the new part" do
+          expect(subject.header[:content_type]).to be_nil
+          expect(subject.header[:content_disposition]).to be_nil
+          expect(subject.header[:content_description]).to be_nil
+          expect(subject.header[:content_transfer_encoding]).to be_nil
+
+          expect(subject.parts[0].header[:content_type].value).to eq("application/x-gzip; NAME=blah.gz")
+          expect(subject.parts[0].header[:content_disposition].value).to eq("attachment; filename=blah.gz")
+          expect(subject.parts[0].header[:content_description].value).to eq("Attachment has identical content to above foo.gz")
+          expect(subject.parts[0].header[:content_transfer_encoding].value).to eq("base64")
+        end
+      end
+
       describe "content-transfer-encoding" do
 
         it "should use 7bit for only US-ASCII chars" do

commit 992e8feafab9902f6a01296845bb6babaed87ff0

Fix regression in content_type for text part after converted to multipart
This was broken by 404ecdd290f89658fb05da02ea5b6139fd82ebdb.  It removed
the setting of the default :content_type entry for the text part, before
copying the content fields to the text part.  This puts back the setting
of the default :content_type entry for the text part.

In 2.7.1:

```ruby
m = Mail.new
m.body 'c'
m.add_file :filename=>'a.txt', :content=>'b'
m.parts.first.content_type =~ /text\/plain/ # => 0
```

Before this commit:

```ruby
m = Mail.new
m.body 'c'
m.add_file :filename=>'a.txt', :content=>'b'
m.parts.first.content_type =~ /text\/plain/ # => nil

m.encoded
m.parts.first.content_type =~ /text\/plain/ # => 0
```

After this commit:

```ruby
m = Mail.new
m.body 'c'
m.add_file :filename=>'a.txt', :content=>'b'
m.parts.first.content_type =~ /text\/plain/ # => 0
```

lib/mail/message.rb
@@ -1769,7 +1769,7 @@ module Mail
     private_constant :MULTIPART_CONVERSION_CONTENT_FIELDS if respond_to?(:private_constant)
 
     def convert_to_multipart
-      text_part = Mail::Part.new(:body => body.decoded)
+      text_part = Mail::Part.new(:content_type => 'text/plain;', :body => body.decoded)
 
       MULTIPART_CONVERSION_CONTENT_FIELDS.each do |field_name|
         if value = send(field_name)

spec/mail/message_spec.rb
@@ -1455,6 +1455,28 @@ describe Mail::Message do
         end
       end
 
+      describe "add_file sets content_type from attachment" do
+        it "should work if set before body" do
+          m = Mail.new
+          m.add_file :filename=>'a.txt', :content=>'b'
+          m.body 'c'
+          expect(m.parts.first.content_type).to match(/text\/plain/)
+          expect(m.parts.first.body.encoded).to eq('b')
+          expect(m.parts.last.content_type).to match(/text\/plain/)
+          expect(m.parts.last.body.encoded).to eq('c')
+        end
+
+        it "should work if set after body" do
+          m = Mail.new
+          m.body 'c'
+          m.add_file :filename=>'a.txt', :content=>'b'
+          expect(m.parts.first.content_type).to match(/text\/plain/)
+          expect(m.parts.first.body.encoded).to eq('c')
+          expect(m.parts.last.content_type).to match(/text\/plain/)
+          expect(m.parts.last.body.encoded).to eq('b')
+        end
+      end
+
       describe "content-transfer-encoding" do
 
         it "should use 7bit for only US-ASCII chars" do